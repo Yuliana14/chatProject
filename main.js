d027355dcc12273714fe2ac37a5a2b00e77f1051
yuls14 <jimenez.yuliana@outlook.es>
2020-06-29 00:48:57 -0700

    first commit chat

---
+++ main.js
@@ -0,0 +1,30 @@
+// const express = require('express');
+// const app = express();
+// var PORT = 8080;
+
+// var server = app.listen(8080)
+// var io = require('socket.io').listen(server);
+
+
+// app.engine('html', require('jade').renderFile);
+// app.set('view options',{
+//     layout:false
+// })
+// app.use(express.static(__dirname+'/static'))
+
+// app.get('/',function (req,res) {
+//     //res.send('Hello guys')
+//     //rendering view
+//     res.render('main.jade');
+// })
+
+// //listen events socket
+// io.sockets.on('connection',function (socket) {
+//     //client variables
+//     console.log('client connected');
+//     socket.on('set_nickname',function (nickname) {
+//         console.log('trying to set nickname',nickname);
+        
+//     })
+    
+// })
---
+++ static/js/chat.js
@@ -0,0 +1,25 @@
+
+$(function () {
+    
+    var socket = io.connect('/');
+
+    socket.on('connect', function(){
+        console.log('Connected with socket');
+        init();
+    });
+
+    var init = function () {
+        $("#nickname").keyup(function (e) {
+            var code = e.which || e.keyCode;
+            if (code == 13) {
+                setNickname = ($(this).val());
+            }
+        })
+    }
+
+    var setNickname = function (nickname) {
+        debugger
+        socket.emit('set_nickname',nickname);
+    }
+    
+})
